5c039878d14f6a27c9b436961ee06520f19e9554
Benjamin Chafe <39773821+bchafe@users.noreply.github.com>
2021-10-15 00:27:51 -0300

    Update libturtle.lua
    add selectSlotMem(), add slot reset in refuel()

--- libturtle.lua
+++ libturtle.lua
@@ -9,18 +9,25 @@ local function getSlotMem()
     return slotMem
 end
 
+local function selectSlotMem()
+    return turtle.select(slotMem)
+end
+
 
 -- refuel(): Looks for coal, attempts to refuel the turtle if found.
 local function refuel()
     if ( turtle.getFuelLevel() == 0 ) then
         for i=1,16 do
             if (type(turtle.getItemDetail(i)) == "table" and turtle.getItemDetail(i)["name"] == "minecraft:coal") then
+                setSlotMem() -- Saves current slot to return to after refueling.
                 turtle.select(i)
-                return turtle.refuel(1)
+                result = turtle.refuel(1)
+                selectSlotMem() -- Resets to slot mem.
+                return result
             end
         end
         return false
     end
 end
 
-return { refuel = refuel, setSlotMem = setSlotMem, getSlotMem = getSlotMem }
+return { refuel = refuel, setSlotMem = setSlotMem, getSlotMem = getSlotMem, selectSlotMem = selectSlotMem }
