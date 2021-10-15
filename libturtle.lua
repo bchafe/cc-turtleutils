36edf260149b86a2d2fcfc8856128b9d519198cf
Benjamin Chafe <39773821+bchafe@users.noreply.github.com>
2021-10-14 23:57:39 -0300

    Update libturtle.lua
    fix refuel function (turtle.select was needed to select the proper inv slot)

--- libturtle.lua
+++ libturtle.lua
@@ -2,7 +2,8 @@ local function refuel()
     if ( turtle.getFuelLevel() == 0 ) then
         for i=1,16 do
             if (type(turtle.getItemDetail(i)) == "table" and turtle.getItemDetail(i)["name"] == "minecraft:coal") then
-                return turtle.refuel(i)
+                turtle.select(i)
+                return turtle.refuel(1)
             end
         end
         return false
