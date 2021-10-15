f1dd5e09cb1190e8b14f5bdb09a5a28705f8f2f4
Benjamin Chafe <39773821+bchafe@users.noreply.github.com>
2021-10-15 12:58:28 -0300

    Update libturtle.lua
    add find() and isInInv() functions

--- libturtle.lua
+++ libturtle.lua
@@ -14,20 +14,46 @@ local function selectSlotMem()
 end
 
 
+-- find(): searches turtle inventory for item given by id, returns inv. index for item if found.
+local function find(id)
+    for i=1,16 do
+        slot = turtle.getItemDetail(i)
+        if (type(slot) == "table" and slot["name"] == id) then
+            return i
+        end
+    end
+    return nil
+end
+
+
+-- isInInv(): Checks to see if an item is in the turtle inv. returns false if not found.
+local function isInInv(id)
+    if type(find(id)) == "number" then
+        return true
+    end
+    return false
+end
+
+
 -- refuel(): Looks for coal, attempts to refuel the turtle if found.
 local function refuel()
     if ( turtle.getFuelLevel() == 0 ) then
-        for i=1,16 do
-            if (type(turtle.getItemDetail(i)) == "table" and turtle.getItemDetail(i)["name"] == "minecraft:coal") then
-                setSlotMem() -- Saves current slot to return to after refueling.
-                turtle.select(i)
-                result = turtle.refuel(1)
-                selectSlotMem() -- Resets to slot mem.
-                return result
-            end
-        end
-        return false
+        setSlotMem()
+        result = turtle.select(find("minecraft:coal"))
+        selectSlotMem()
+        return result
     end
 end
 
-return { refuel = refuel, setSlotMem = setSlotMem, getSlotMem = getSlotMem, selectSlotMem = selectSlotMem }
+
+--dig(): digs until the block is confirmed broken (sand/gravel safe).
+--local function dig
+
+return { 
+    refuel = refuel, 
+    setSlotMem = setSlotMem, 
+    getSlotMem = getSlotMem, 
+    selectSlotMem = selectSlotMem,
+    find = find, 
+    isInInv = isInInv 
+}
